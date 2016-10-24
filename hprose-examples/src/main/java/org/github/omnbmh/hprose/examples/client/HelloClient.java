6694d7ba1bfc2b7341dd18af9dc9e4ad0e07b968
dezhichen <c8d8z8@gmail.com>
2016-10-24 17:49:40 +0800

    默认示例通了

--- hprose-examples/src/main/java/org/github/omnbmh/hprose/examples/client/HelloClient.java
+++ hprose-examples/src/main/java/org/github/omnbmh/hprose/examples/client/HelloClient.java
@@ -1,6 +1,7 @@
 package org.github.omnbmh.hprose.examples.client;
 
 import hprose.client.HproseHttpClient;
+import hprose.client.HproseTcpClient;
 
 import java.io.IOException;
 
@@ -9,11 +10,29 @@ import java.io.IOException;
  */
 public class HelloClient {
     public static void main(String[] args) throws Throwable {
+        httpReq();
+        // 未通
+        //webSocketReq();
+    }
+
+
+    static void webSocketReq() throws Throwable {
+        HproseTcpClient client =  new HproseTcpClient();
+        client.useService("http://localhost:8080/hprose-examples/wshello");
+        String result = (String) client.invoke("sayHello", new Object[]{"Hprose"});
+        System.out.println(result);
+        result = (String) client.invoke("sayHello", new Object[]{"中国"});
+        System.out.println(result);
+
+    }
+
+    static void httpReq() throws Throwable {
         HproseHttpClient client = new HproseHttpClient();
         client.useService("http://localhost:8080/hprose-examples/Hello");
-        String result = (String) client.invoke("sayHello", new Object[] { "Hprose" });
+        String result = (String) client.invoke("sayHello", new Object[]{"Hprose"});
         System.out.println(result);
-        result = (String) client.invoke("sayHello", new Object[] { "中国" });
+        result = (String) client.invoke("sayHello", new Object[]{"中国"});
         System.out.println(result);
+
     }
 }
